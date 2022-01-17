c75aec9cdb10a004c84ef440ddd5370753070a09
Asakur <40217318+Asakur-315@users.noreply.github.com>
2022-01-17 11:32:21 +0300

    Начало 7 практики

---
+++ 7 Параллельное программирование/7pract.py
@@ -0,0 +1,10 @@
+import multiprocessing as mp
+import yaml
+import random
+from typing import List
+
+MATRIX1_FILE = "./matrix1.yml"
+MATRIX2_FILE = "./matrix2.yml"
+RESULT_FILE = "./result.yml"
+
+
